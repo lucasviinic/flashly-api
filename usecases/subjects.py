2b105a95fa99ac7183f87bc27db1f24c15e863b4
Lucas Vinicius <lvms@cin.ufpe.br>
2025-05-19 22:25:22 -0300

    WIP

--- usecases/subjects.py
+++ usecases/subjects.py
@@ -10,19 +10,19 @@ from models.session_model import Sessions
 from models.subject_model import Subjects
 from models.topic_model import Topics
 from models.user_model import Users
-from utils.constants import USER_LIMITS
+from services.limit_service import LimitService
 
 
 def retrieve_all_subjects_usecase(db: db_dependency, user_id: str, limit: int, offset: int, search: str) -> List[dict]:
     query = db.query(Subjects).filter(Subjects.user_id == user_id).filter(Subjects.deleted_at == None)
-    
+
     if search:
         query = query.filter(Subjects.subject_name.ilike(f"%{search}%"))
 
     subjects = query.offset(offset).limit(limit).all()
-    
+
     result = []
-    
+
     for subject in subjects:
         topics_with_count = db.query(
             Topics,
@@ -34,13 +34,13 @@ def retrieve_all_subjects_usecase(db: db_dependency, user_id: str, limit: int, o
             Topics.subject_id == subject.id,
             Topics.deleted_at.is_(None)
         ).group_by(Topics.id).all()
-        
+
         topics_list = []
         for topic, count in topics_with_count:
             topic_dict = topic.to_dict()
             topic_dict['count'] = count
             topics_list.append(topic_dict)
-        
+
         result.append({
             "id": subject.id,
             "user_id": subject.user_id,
@@ -51,20 +51,17 @@ def retrieve_all_subjects_usecase(db: db_dependency, user_id: str, limit: int, o
             "deleted_at": subject.deleted_at,
             "topics": topics_list
         })
-    
+
     return result
 
+
 def create_subject_usecase(db: db_dependency, subject_request: SubjectRequest, user_id: str) -> dict:
-    total_subjects = db.query(Subjects).filter(
-        Subjects.user_id == user_id,
-        Subjects.deleted_at.is_(None)
-    ).count()
-
     user = db.query(Users).filter(Users.id == user_id, Users.deleted_at.is_(None)).first()
-    subjects_limit = USER_LIMITS[user.account_type]["subjects_limit"]
-
-    if total_subjects >= subjects_limit:
-        raise HTTPException(status_code=400, detail='Subject limit reached')
+    if not user:
+        raise HTTPException(status_code=404, detail='User not found')
+    
+    limit_service = LimitService(db, user, Flashcards, Subjects)
+    limit_service.check_subject_quota()
 
     subject_model = Subjects(**subject_request.model_dump(), user_id=user_id)
 
@@ -76,13 +73,14 @@ def create_subject_usecase(db: db_dependency, subject_request: SubjectRequest, u
 
     return result
 
+
 def update_subject_usecase(db: db_dependency, subject_request: SubjectRequest, subject_id: str, user_id: str) -> dict:
     subject_model = db.query(Subjects).filter(Subjects.id == subject_id)\
         .filter(Subjects.user_id == user_id).first()
-    
+
     if not subject_model:
         raise HTTPException(status_code=404, detail='subject not found')
-    
+
     subject_model.subject_name = subject_request.subject_name
     subject_model.image_url = subject_request.image_url
     subject_model.updated_at = datetime.now(timezone.utc)
@@ -94,27 +92,29 @@ def update_subject_usecase(db: db_dependency, subject_request: SubjectRequest, s
 
     return result
 
+
 def retrieve_subject_usecase(db: db_dependency, subject_id: str, user_id: str) -> dict:
     subject_model = db.query(Subjects).filter(Subjects.id == subject_id)\
         .filter(Subjects.user_id == user_id).filter(Subjects.deleted_at == None).first()
 
     if not subject_model:
         raise HTTPException(status_code=404, detail='subject not found')
-    
+
     result = subject_model.to_dict()
 
     return result
 
+
 def delete_subject_usecase(db: db_dependency, subject_id: str, user_id: str) -> None:
     subject_model = db.query(Subjects).filter(Subjects.id == subject_id)\
         .filter(Subjects.user_id == user_id).first()
-    
+
     if not subject_model:
         raise HTTPException(status_code=404, detail='subject not found')
-        
+
     db.query(Sessions).filter(Sessions.subject_id == subject_id).delete()
     db.query(Flashcards).filter(Flashcards.subject_id == subject_id).delete()
     db.query(Topics).filter(Topics.subject_id == subject_id).delete()
-    
+
     db.delete(subject_model)    
     db.commit()
